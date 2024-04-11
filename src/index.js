3a35420f1724ad3b7e523e6464410709d3c93d73
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2024-04-11 00:08:10 -0400

    counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("starting up!");
+import $ from "jquery";
+
+// $("#main").html("Here we go!");
+
+let count = 0;
+
+const updateCounter = () => {
+  $("#main").text(`You've been on this page for ${count} seconds.`);
+  count++;
+};
+
+setInterval(updateCounter, 1000);

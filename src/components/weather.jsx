42683e92bf6afd9f249e924280fb5d5b7f8e1650
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2025-11-08 12:49:32 -0800

    Removing hard coding for searching by city name

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function Weather() {
   const [weather, setWeather] = useState(null);
+  const [cityName, setCityName] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
@@ -16,8 +17,21 @@ function Weather() {
 
       const API_KEY = import.meta.env.VITE_OPENWEATHER_KEY;
 
+      const coordRes = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`,
+      );
+
+      if (!coordRes.ok) {
+        throw new Error('ERROR FETCHING COORDINATES');
+      }
+
+      const coordData = await coordRes.json();
+      console.log(coordData);
+      setCityName(coordData[0].name);
+
+      /* Using coordinates, fetch weather data */
       const res = await fetch( // hardcoded longitute/ latitude for now
-        `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&units=imperial&appid=${API_KEY}`,
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${coordData[0].lat}&lon=${coordData[0].lon}&units=imperial&appid=${API_KEY}`,
       );
 
       if (!res.ok) {
@@ -64,8 +78,7 @@ function Weather() {
 
       {!loading && !error && weather && (
         <div className="weather-card">
-          <h2>Coordinates</h2>
-          <p><em>({weather.lat}, {weather.lon})</em></p>
+          <p>Showing results for: {cityName}</p>
           <h2>Temperature</h2>
           <p>{weather.current.temp}°F</p>
           <p><em>Feels like:</em> {weather.current.feels_like}°F</p>

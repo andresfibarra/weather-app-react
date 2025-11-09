984a3248ab2a252a815249349aebe7c6dd3f4ab8
andresfibarra <andres.f.ibarra.24@dartmouth.edu>
2025-11-08 16:28:36 -0800

    fleshing out about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,22 @@ function About(props) {
   return (
     <div className="page-card">
       <h1>About this app</h1>
-      <p>All there is to know about me</p>
+      <p>This is a simple app that calls weather
+        the OpenWeather API to get information on the weather for a given place
+      </p>
+      <h3>Inspiration</h3>
+      <p>
+        I was inspired to do this by curiosity on learning how to use APIs!
+        I was a little out of practice with frontend coding, and so I wanted to brush up
+      </p>
+      <h3>Goals</h3>
+      <ol>
+        <li>Brush up on React and frontend frameworks</li>
+        <li>Learn!</li>
+        <li>Learn to love coding again</li>
+        <li>Push myself to practice problem solving and incremental building</li>
+        <li>Practice good coding habits</li>
+      </ol>
     </div>
   );
 }
